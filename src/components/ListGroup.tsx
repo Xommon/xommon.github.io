a471a2be32bec6c6ce14ccb1b206f2aab8f32003
Micheal Quentin <mquenti3@gmail.com>
2024-05-11 15:51:10 -0500

    Dynamic Selection

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,10 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["Chicago", "Berlin", "Montréal", "Vancouver", "Barcelona"];
 
-  // Event handler
-  const handleClick = (e: MouseEvent) => console.log(e);
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -12,7 +12,17 @@ function ListGroup() {
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
