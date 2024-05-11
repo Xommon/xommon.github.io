aaf50ab1be8d9e941d4d8d11a7f09fa13d876aaa
Micheal Quentin <mquenti3@gmail.com>
2024-05-11 14:34:15 -0500

    Event Handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  let items = ["Chicago", "Berlin", "Montreal", "Vancouver", "Barcelona"];
-  items = [];
+  let items = ["Chicago", "Berlin", "Montréal", "Vancouver", "Barcelona"];
+
+  // Event handler
+  const handleClick = (e: MouseEvent) => console.log(e);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
