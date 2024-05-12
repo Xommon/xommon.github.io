05ac29d97a716c7f4f9ff3d621172c29a39bd9fc
Micheal Quentin <mquenti3@gmail.com>
2024-05-12 07:52:59 -0500

    Change Project

--- src/Message.tsx
+++
@@ -1,9 +0,0 @@
-function Message() {
-    const name = 'Micheal';
-    if (name)
-        return <h1>Hello, {name}!</h1>;
-    else
-        return <h1>Hello, World!</h1>
-}
-
-export default Message;
